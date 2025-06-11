b99e0cabf29bd216a4099f7046fdf0eb3762083c
yeison <yeison@yeisontech.com>
2025-06-11 17:50:26 -0500

    Estilos adelantados

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom"
 
 import { Home } from "./components/home/home"
+import NavBar from "./components/navBar/NavBar"
 
 function App() {
 
   return (
     <>
       <Router>
+
+          < NavBar />
+
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -1,11 +1,40 @@
-import React from 'react'
-
-import "./navBar.css"
+import React from "react";
+import "./navBar.css";
+import logoLive from "../../assets/logoLive.png";
+import logoDark from "../../assets/logoDark.png";
 
 const NavBar = () => {
   return (
-    <div>barra de navegacion</div>
-  )
-}
+    <nav>
+      <img src={logoLive} alt="logo" className="logo" />
 
-export default NavBar
+      <ul>
+        <li>
+          <a href="#">Home</a>
+        </li>
+
+        <li>
+          <a href="#">Sobre mi</a>
+        </li>
+
+        <li>
+          <a href="#">Habilidades</a>
+        </li>
+
+        <li>
+          <a href="#">Portafolio</a>
+        </li>
+
+        <li>
+          <a href="#">Contacto</a>
+        </li>
+
+        <i className="fa-solid fa-xmark"></i>
+      </ul>
+
+      <i className="fa-solid fa-bars"></i>
+    </nav>
+  );
+};
+
+export default NavBar;
