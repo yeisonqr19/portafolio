00a8626b0d1829f397cc8e8869ab2af203169003
yeison <yeison@yeisontech.com>
2025-06-16 12:42:48 -0500

    Error Corregido

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import { Home } from "./components/home/home";
+import { Home } from "./components/home/Home";
 import NavBar from "./components/navBar/NavBar";
 import { TemaProvider } from "./temaContext/TemaContext";
 import { Footer } from "./components/footer/Footer";
